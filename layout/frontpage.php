799ad75baf07f412f9bb49e8ba61689a08c279cc
José Espinoza <josespinoza@alumnos.uai.cl>
2017-01-04 16:41:56 -0300

    Link a fitnessgram

--- layout/frontpage.php
+++ layout/frontpage.php
@@ -54,6 +54,54 @@ if (!empty($PAGE->theme->settings->infobox)) {
 }
 ?>
 
+<style>
+.container {
+    position: relative;
+    width: 50%;
+}
+
+.image {
+  opacity: 1;
+  display: block;
+  width: 100%;
+  height: auto;
+  transition: .5s ease;
+  backface-visibility: hidden;
+  border-radius: 15px;
+}
+
+.middle {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%)
+}
+
+.container:hover .image {
+  opacity: 0.3;
+}
+
+.container:hover .middle {
+  opacity: 1;
+}
+
+.text {
+  background-color: #851924;
+  color: white;
+  font-size: 45px;
+  padding: 20px 40px;
+}
+</style>
+<div class="container">
+  <img src="http://www.uai.cl/images/sitio/alumnos_pregrado/infraestructura.jpg" alt="Avatar" class="image" style="width:100%">
+  <div class="middle">
+    <div class="text">Revisa acá tu FITNESSGRAM </div>
+  </div>
+</div>
+
 <?php if (!empty($PAGE->theme->settings->frontpagemarketenabled)) {
     if (isloggedin())
     	echo $OUTPUT->get_marketing_blocks();
@@ -89,7 +137,6 @@ if (!empty($PAGE->theme->settings->infobox2)) {
 }
 }
 ?>
-
 <div class="container outercont">
     <div id="page-content" class="row-fluid">
      <div id="page-navbar" class="span12">
